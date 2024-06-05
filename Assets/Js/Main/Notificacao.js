3d027da1ad881b2c0d1b8c47998d17b1fe173c05
wender103 <wendertestes01@gmail.com>
2024-06-05 20:15:56 -0300

    pequeno bug nas notificações

--- Assets/Js/Main/Notificacao.js
+++ Assets/Js/Main/Notificacao.js
@@ -13,6 +13,7 @@ function Noficacao_Classes(Classe) {
 
 let array_notificacoes_nao_vistas = []
 function Carregar_Notificacaoes() {
+    array_notificacoes_nao_vistas = []
     if(User.Notificacao.length > 0 ) {
         setTimeout(() => {
             if(!noficacao_esta_aberta) {
@@ -22,19 +23,19 @@ function Carregar_Notificacaoes() {
                     }
                 }
     
-                let contador = 0
                 function Mostrar_Notificacoes() {
-                    let texto = `<h1>${array_notificacoes_nao_vistas[contador].Titulo}</h1>
-                    <p>${array_notificacoes_nao_vistas[contador].Mensagem}</p>`
-                    Notificar_Infos(texto, array_notificacoes_nao_vistas[contador].Config.Comando, array_notificacoes_nao_vistas[contador].Config.Texto_Btn, array_notificacoes_nao_vistas[contador].Config.Link).then((resp) => {
+                    let texto = `<h1>${array_notificacoes_nao_vistas[0].Titulo}</h1>
+                    <p>${array_notificacoes_nao_vistas[0].Mensagem}</p>`
+                    Notificar_Infos(texto, array_notificacoes_nao_vistas[0].Config.Comando, array_notificacoes_nao_vistas[0].Config.Texto_Btn, array_notificacoes_nao_vistas[0].Config.Link).then((resp) => {
                         if(resp) {
-                            if(array_notificacoes_nao_vistas[contador].Extra == 'Postar Música') {
+                            if(array_notificacoes_nao_vistas[0].Extra == 'Postar Música') {
                                 document.getElementById('input_add_musica').value = document.getElementById('link_notificacao').href
                                 Postar_Musica('Abrir Página: adicionarmusicas')   
                             }
                         }
-    
-                        Remover_Notificacao_User(array_notificacoes_nao_vistas[contador])
+                        
+                        Remover_Notificacao_User(array_notificacoes_nao_vistas[0])
+                        Carregar_Notificacaoes()
                     })
                 } Mostrar_Notificacoes()
             } else {
